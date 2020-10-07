e68f0af634d492619f650b5eacd850bca1e732da
n3x <spueschel89@gmail.com>
2020-10-07 08:05:46 +0200

    Added openapi definition file

---
+++ openapi/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Get current OpenAPI definition file from the DFIRTrack repo
+wget https://raw.githubusercontent.com/stuhli/dfirtrack/develop/dfirtrack_api/openapi/openapi_dfirtrack.yml .
