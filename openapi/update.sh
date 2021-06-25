74adbdee8b9b3a1679644a6090f667e7acc2a044
n3x <spueschel89@gmail.com>
2021-06-25 08:03:48 +0200

    update.sh change url to new dfirtrack org

--- openapi/update.sh
+++ openapi/update.sh
@@ -2,4 +2,4 @@
 
 #Get current OpenAPI definition file from the DFIRTrack repo
 rm openapi_dfirtrack.yml
-wget https://raw.githubusercontent.com/stuhli/dfirtrack/develop/dfirtrack_api/openapi/openapi_dfirtrack.yml .
+wget https://raw.githubusercontent.com/dfirtrack/dfirtrack/develop/dfirtrack_api/openapi/openapi_dfirtrack.yml .
